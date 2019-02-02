f1916f4bc5996710f3717485f9bfca60b7bf341f
furiaruchit <furiaruchit@gmail.com>
2019-02-02 22:49:42 +0530

    Third commit

--- app/src/main/java/com/example/hackit/First.java
+++ app/src/main/java/com/example/hackit/First.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,12 +28,11 @@ import java.util.List;
 public class First extends AppCompatActivity {
 
     int size;
+    int sumincome = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-
-
 
         Button analysis = findViewById(R.id.checkanalysis);
         analysis.setOnClickListener(new View.OnClickListener() {
@@ -52,7 +52,22 @@ public class First extends AppCompatActivity {
                 final String income_src = e1.getText().toString();
                 EditText e2 = findViewById(R.id.incomeamt);
                 final String income_amt = e2.getText().toString();
-                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/Income/").push();
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/Income/");
+//                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+//                            String inc = dataSnapshot1.child("Amount").getValue().toString();
+//                            sumincome += Float.parseFloat(inc);
+//                            TextView income = findViewById(R.id.income);
+//                            income.setText("Rs " + sumincome);
+//                        }
+//                    }
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
                 ref.child("Income Source").setValue(income_src);
                 ref.child("Amount").setValue(income_amt);
             }
@@ -67,6 +82,10 @@ public class First extends AppCompatActivity {
                 String expenditure_src = e3.getText().toString();
                 EditText e4 = findViewById(R.id.expenditureamt);
                 String expenditure_amt = e4.getText().toString();
+//                int sumexpenditure = 0;
+//                sumexpenditure += Float.parseFloat(expenditure_amt);
+//                TextView income = findViewById(R.id.expenditure);
+//                income.setText("Rs " + sumexpenditure);
                 List<String[]> csvContent = readCVSFromAssetFolder();
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
                 String format = simpleDateFormat.format(new Date());
@@ -74,7 +93,7 @@ public class First extends AppCompatActivity {
                 {
                     String [] rows = csvContent.get(i);
                     Log.d("First",rows[0]+rows[1]);
-                    if(expenditure_src.equals(rows[0]))
+                    if(expenditure_src.equalsIgnoreCase(rows[0]))
                     {
                         expenditure_src= rows[1];
                     }
@@ -114,5 +133,4 @@ public class First extends AppCompatActivity {
         }
         return csvLine;
     }
-
 }
