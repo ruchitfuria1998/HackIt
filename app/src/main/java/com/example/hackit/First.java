e81426578eccfb7ea7f120b58cdd863eb288bb75
furiaruchit <furiaruchit@gmail.com>
2019-02-02 20:15:24 +0530

    Second commit

--- app/src/main/java/com/example/hackit/First.java
+++ app/src/main/java/com/example/hackit/First.java
@@ -1,6 +1,7 @@
 package com.example.hackit;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +9,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class First extends AppCompatActivity {
 
+    int size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+
 
         Button analysis = findViewById(R.id.checkanalysis);
         analysis.setOnClickListener(new View.OnClickListener() {
@@ -30,25 +49,70 @@ public class First extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText e1 = findViewById(R.id.incomesrc);
-                String income_src = e1.getText().toString();
+                final String income_src = e1.getText().toString();
                 EditText e2 = findViewById(R.id.incomeamt);
-                String income_amt = e2.getText().toString();
+                final String income_amt = e2.getText().toString();
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/Income/").push();
+                ref.child("Income Source").setValue(income_src);
+                ref.child("Amount").setValue(income_amt);
             }
         });
-
 
         Button expendituresub = findViewById(R.id.expendituresub);
         expendituresub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int i;
                 EditText e3 = findViewById(R.id.expendituresrc);
                 String expenditure_src = e3.getText().toString();
                 EditText e4 = findViewById(R.id.expenditureamt);
                 String expenditure_amt = e4.getText().toString();
+                List<String[]> csvContent = readCVSFromAssetFolder();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+                String format = simpleDateFormat.format(new Date());
+                for (i=0;i<csvContent.size();i++)
+                {
+                    String [] rows = csvContent.get(i);
+                    Log.d("First",rows[0]+rows[1]);
+                    if(expenditure_src.equals(rows[0]))
+                    {
+                        expenditure_src= rows[1];
+                    }
+                }
+                final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/Expenditure/"+expenditure_src);
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        size = (int) dataSnapshot.getChildrenCount();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+                ref.child(format).setValue(expenditure_amt);
             }
         });
 
-
+    }
+    private List<String[]> readCVSFromAssetFolder(){
+        List<String[]> csvLine = new ArrayList<>();
+        String[] content;
+        try {
+            InputStream inputStream = getResources().getAssets().open("expenditureSource.csv");
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while((line = br.readLine()) != null){
+                content = line.split(",");
+                csvLine.add(content);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return csvLine;
     }
 
 }
